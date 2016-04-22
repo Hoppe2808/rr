21accd6a6e0feb5b37b09886c4ce7b365a93da64
SebbyG <seb.hop@hotmail.com>
2016-04-22 11:11:04 +0200

    dkmsa

--- src/tempName/Ricochet.java
+++ src/tempName/Ricochet.java
@@ -3,207 +3,291 @@ package tempName;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.awt.Point;
 
 public class Ricochet {
 
-    public static void main(String[] args) throws IOException {
-        Board board = new Ricochet().loadBoard();
+	public static void main(String[] args) throws IOException {
+		Board board = new Ricochet().loadBoard();
 
-        // Debug code - uncomment to test that robot 0 is moved as expected
-        System.out.print(board);
+		// Debug code - uncomment to test that robot 0 is moved as expected
+		System.out.print(board);
 
-        board.computeSolution();
-        System.out.println("Possible endpoints for robot 0: " + board.possibleEndpointsForRobot(0));
+		board.computeSolution();
+		System.out.println("Possible endpoints for robot 0: " + board.possibleEndpointsForRobot(0));
 
-    }
+	}
 
-    public class Board {
-        private static final char GOAL_CHAR = 'G';
-        private static final char WALL_CHAR = '#';
-        private static final char EMPTY_CHAR = ' ';
+	public class Board {
+		private static final char GOAL_CHAR = 'G';
+		private static final char WALL_CHAR = '#';
+		private static final char EMPTY_CHAR = ' ';
 
-        private char[][] board;
-        private int size;
-        private Point[] robots;
-        private Point goal;
-        private Point previousPosition;
-        private Point deadEnd;
+		private char[][] board;
+		private int size;
+		private Point[] robots;
+		private Point goal;
+		private Point previousPosition;
+		private Point deadEnd;
+		private ArrayList<Point> vP = new ArrayList<Point>();
+		private String prevDirection;
+		private int loopDeadEnd;
 
-        public Board(char[][] board, Point[] robots, Point goal) {
-            this.board = board;
-            this.size = board.length;
-            this.robots = robots;
-            this.goal = goal;
-        }
+		public Board(char[][] board, Point[] robots, Point goal) {
+			this.board = board;
+			this.size = board.length;
+			this.robots = robots;
+			this.goal = goal;
+		}
 
-        public void computeSolution() {
-            
-        	while(!(robots[0].equals(goal))){
-        		try {
-        		    Thread.sleep(2000);                 //1000 milliseconds is one second.
-        		} catch(InterruptedException ex) {
-        		    Thread.currentThread().interrupt();
-        		}
-            	if (!(pointAfterMovingRobot(0, Direction.Up).equals(robots[0])) && !(pointAfterMovingRobot(0, Direction.Up).equals(previousPosition))){
-            		if (!(pointAfterMovingRobot(0, Direction.Up).equals(deadEnd))){
-	            		previousPosition = robots[0];
-	            		moveRobot(0, Direction.Up);
-	            		System.out.println("Up");
-            		}
-            	} else if(!(pointAfterMovingRobot(0, Direction.Down).equals(robots[0])) && !(pointAfterMovingRobot(0, Direction.Down).equals(previousPosition))){
-            		if (!(pointAfterMovingRobot(0, Direction.Down).equals(deadEnd))){
-	            		previousPosition = robots[0];
-	            		moveRobot(0, Direction.Down);
-	            		System.out.println("Down");
-            		}
-            	} else if(!(pointAfterMovingRobot(0, Direction.Left).equals(robots[0])) && !(pointAfterMovingRobot(0, Direction.Left).equals(previousPosition))){
-            		if (!(pointAfterMovingRobot(0, Direction.Left).equals(deadEnd))){
-	            		previousPosition = robots[0];
-	            		moveRobot(0, Direction.Left);
-	            		System.out.println("Left");
-            		}
-            	} else if(!(pointAfterMovingRobot(0, Direction.Right).equals(robots[0])) && !(pointAfterMovingRobot(0, Direction.Right).equals(previousPosition))){
-            		if (!(pointAfterMovingRobot(0, Direction.Right).equals(deadEnd))){
-	            		previousPosition = robots[0];
-	            		moveRobot(0, Direction.Right);
-	            		System.out.println("Right");
-            		}
-            	} else{
-            		deadEnd = robots[0];
-            		previousPosition = null;
-            	}
-        	}
-        	System.out.println("Win");
-        }
+		public void computeSolution() {
 
-        public Endpoints possibleEndpointsForRobot(int robot) {
-            assert robot < robots.length;
+			while(!(robots[0].equals(goal))){
+				try {
+					Thread.sleep(1000);                 //1000 milliseconds is one second.
+				} catch(InterruptedException ex) {
+					Thread.currentThread().interrupt();
+				}
+//				int dirCount0;
+//				int dirCount1;
+//				int dirCount2;
+//				int dirCount3;
+//
+//				dirCount0 = (int) (Math.random() * 4) + 1;
+//
+//				do {
+//					dirCount1 = (int) (Math.random() * 4) + 1;
+//				} while (dirCount0 == dirCount1);
+//
+//				do {
+//					dirCount2 = (int) (Math.random() * 4) + 1;
+//				} while (dirCount0 == dirCount2 || dirCount1 == dirCount2);
+//
+//				do {
+//					dirCount3 = (int) (Math.random() * 4) + 1;
+//				} while (dirCount0 == dirCount3 || dirCount1 == dirCount3 || dirCount2 == dirCount3);
+				
+				loopDeadEnd = 0;
+				
+				if (!(pointAfterMovingRobot(0, Direction.Up).equals(robots[0])) && !(pointAfterMovingRobot(0, Direction.Up).equals(previousPosition))){
+					if (!(pointAfterMovingRobot(0, Direction.Up).equals(deadEnd))){
+						boolean move = true;
+						for (int i = 0; i < vP.size(); i++){
+							if (vP.get(i).equals(pointAfterMovingRobot(0, Direction.Up))){
+								loopDeadEnd++;
+								move = false;
+							}
+						}
+						if (move){
+						previousPosition = robots[0];
+						moveRobot(0, Direction.Up);
+						vP.add(robots[0]);
+						prevDirection = "up";
+						System.out.println("Up");
+						}
+					}
+				} else if(!(pointAfterMovingRobot(0, Direction.Down).equals(robots[0])) && !(pointAfterMovingRobot(0, Direction.Down).equals(previousPosition))){
+					if (!(pointAfterMovingRobot(0, Direction.Down).equals(deadEnd))){
+						boolean move = true;
+						for (int i = 0; i < vP.size(); i++){
+							if (vP.get(i).equals(pointAfterMovingRobot(0, Direction.Down))){
+								loopDeadEnd++;
+								move = false;
+							}
+						}
+						if (move){
+							previousPosition = robots[0];
+							moveRobot(0, Direction.Down);
+							vP.add(robots[0]);
+							prevDirection = "down";
+							System.out.println("Down");
+						}
+					}
+				} else if(!(pointAfterMovingRobot(0, Direction.Left).equals(robots[0])) && !(pointAfterMovingRobot(0, Direction.Left).equals(previousPosition))){
+					if (!(pointAfterMovingRobot(0, Direction.Left).equals(deadEnd))){
+						boolean move = true;
+						for (int i = 0; i < vP.size(); i++){
+							if (vP.get(i).equals(pointAfterMovingRobot(0, Direction.Left))){
+								loopDeadEnd++;
+								move = false;
+							}
+						}
+						if (move){
+							previousPosition = robots[0];
+							moveRobot(0, Direction.Left);
+							vP.add(robots[0]);
+							prevDirection = "left";
+							System.out.println("Left");
+						}
+					}
+				} else if(!(pointAfterMovingRobot(0, Direction.Right).equals(robots[0])) && !(pointAfterMovingRobot(0, Direction.Right).equals(previousPosition))){
+					if (!(pointAfterMovingRobot(0, Direction.Right).equals(deadEnd))){
+						boolean move = true;
+						for (int i = 0; i < vP.size(); i++){
+							if (vP.get(i).equals(pointAfterMovingRobot(0, Direction.Right))){
+								loopDeadEnd++;
+								move = false;
+							}
+						}
+						if (move){
+							previousPosition = robots[0];
+							moveRobot(0, Direction.Right);
+							vP.add(robots[0]);
+							prevDirection = "right";
+							System.out.println("Right");
+						}
+					}
+				} else{
+					for (int i = 0; i < vP.size(); i++){
+						System.out.println("dsads");
+						if (vP.get(i).equals(robots[0])){
+							moveBackRobot(0, vP.get(i-1));
+							System.out.println("Kører jeg?");
+						}
+					}
+				}
 
-            Point up = pointAfterMovingRobot(robot, Direction.Up);
-            Point down = pointAfterMovingRobot(robot, Direction.Down);
-            Point left = pointAfterMovingRobot(robot, Direction.Left);
-            Point right = pointAfterMovingRobot(robot, Direction.Right);
+			}
+			System.out.println("Win");
+		}
 
-            return new Endpoints(up, down, left, right);
-        }
+		public Endpoints possibleEndpointsForRobot(int robot) {
+			assert robot < robots.length;
 
-        public Point pointAfterMovingRobot(int robot, Direction m) {
-            assert robot < robots.length;
+			Point up = pointAfterMovingRobot(robot, Direction.Up);
+			Point down = pointAfterMovingRobot(robot, Direction.Down);
+			Point left = pointAfterMovingRobot(robot, Direction.Left);
+			Point right = pointAfterMovingRobot(robot, Direction.Right);
 
-            Point pos = new Point(robots[robot]);
-            int drow = 0, dcol = 0;
+			return new Endpoints(up, down, left, right);
+		}
 
-            if (m == Direction.Up) {
-                drow = -1;
-            } else if (m == Direction.Down) {
-                drow = 1;
-            }
+		public Point pointAfterMovingRobot(int robot, Direction m) {
+			assert robot < robots.length;
 
-            if (m == Direction.Left) {
-                dcol = -1;
-            } else if (m == Direction.Right) {
-                dcol = 1;
-            }
+			Point pos = new Point(robots[robot]);
+			int drow = 0, dcol = 0;
 
-            while (withinBoard(pos.x+drow, pos.y+dcol) &&
-                   (board[pos.x+drow][pos.y+dcol] == EMPTY_CHAR
-                    || board[pos.x+drow][pos.y+dcol] == GOAL_CHAR)) {
-                pos.translate(drow, dcol);
-            }
+			if (m == Direction.Up) {
+				drow = -1;
+			} else if (m == Direction.Down) {
+				drow = 1;
+			}
 
-            return pos;
-        }
+			if (m == Direction.Left) {
+				dcol = -1;
+			} else if (m == Direction.Right) {
+				dcol = 1;
+			}
 
-        public void moveRobot(int robot, Direction d) {
-            assert robot < robots.length;
+			while (withinBoard(pos.x+drow, pos.y+dcol) &&
+					(board[pos.x+drow][pos.y+dcol] == EMPTY_CHAR
+					|| board[pos.x+drow][pos.y+dcol] == GOAL_CHAR)) {
+				pos.translate(drow, dcol);
+			}
 
-            Point from = robots[robot];
-            Point to = pointAfterMovingRobot(robot, d);
-            board[from.x][from.y] = EMPTY_CHAR;
-            robots[robot] = to;
-            board[to.x][to.y] = (char) ('0' + robot);
-        }
+			return pos;
+		}
 
-        private boolean withinBoard(int x, int y) {
-            return x >= 0 && x < size && y >= 0 && y < size;
-        }
+		public void moveRobot(int robot, Direction d) {
+			assert robot < robots.length;
 
-        public String toString() {
-            StringBuilder sb = new StringBuilder(size * size);
+			Point from = robots[robot];
+			Point to = pointAfterMovingRobot(robot, d);
+			board[from.x][from.y] = EMPTY_CHAR;
+			robots[robot] = to;
+			board[to.x][to.y] = (char) ('0' + robot);
+		}
+		public void moveBackRobot(int robot, Point d) {
+			assert robot < robots.length;
 
-            for (int i = 0; i < board.length; i++) {
-                sb.append(board[i]);
-                sb.append(System.getProperty("line.separator"));
-            }
+			Point from = robots[robot];
+			Point to = d;
+			board[from.x][from.y] = EMPTY_CHAR;
+			robots[robot] = to;
+			board[to.x][to.y] = (char) ('0' + robot);
+		}
 
-            return sb.toString();
-        }
-    }
+		private boolean withinBoard(int x, int y) {
+			return x >= 0 && x < size && y >= 0 && y < size;
+		}
 
-    public enum Direction {
-        Up, Down, Left, Right;
+		public String toString() {
+			StringBuilder sb = new StringBuilder(size * size);
 
-        public String toString() {
-            switch (this) {
-            case Up: return "U";
-            case Down: return "D";
-            case Left: return "L";
-            case Right: return "R";
-            default: throw new AssertionError();
-            }
-        }
-    }
+			for (int i = 0; i < board.length; i++) {
+				sb.append(board[i]);
+				sb.append(System.getProperty("line.separator"));
+			}
 
-    public class Endpoints {
-        public final Point up, down, left, right;
+			return sb.toString();
+		}
+	}
 
-        public Endpoints(Point up, Point down, Point left, Point right) {
-            this.up = up;
-            this.down = down;
-            this.left = left;
-            this.right = right;
-        }
+	public enum Direction {
+		Up, Down, Left, Right;
 
-        public String toString() {
-            return "Endpoints (Up: " + up + ", Down: " + down + ", Left: " + left + ", Right: " + right + ")";
-        }
-    }
+		public String toString() {
+			switch (this) {
+			case Up: return "U";
+			case Down: return "D";
+			case Left: return "L";
+			case Right: return "R";
+			default: throw new AssertionError();
+			}
+		}
+	}
+
+	public class Endpoints {
+		public final Point up, down, left, right;
+
+		public Endpoints(Point up, Point down, Point left, Point right) {
+			this.up = up;
+			this.down = down;
+			this.left = left;
+			this.right = right;
+		}
+
+		public String toString() {
+			return "Endpoints (Up: " + up + ", Down: " + down + ", Left: " + left + ", Right: " + right + ")";
+		}
+	}
 
 
-    private Board loadBoard() throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	private Board loadBoard() throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-        int size = Integer.parseInt(in.readLine());
-        int numberOfRobots = Integer.parseInt(in.readLine());
+		int size = Integer.parseInt(in.readLine());
+		int numberOfRobots = Integer.parseInt(in.readLine());
 
-        char[][] b = new char[size][size];
-        Point[] robots = new Point[numberOfRobots];
+		char[][] b = new char[size][size];
+		Point[] robots = new Point[numberOfRobots];
 
-        Point goal = null;
+		Point goal = null;
 
-        for (int i = 0; i < size; i++) {
-            char[] line = in.readLine().toCharArray();
-            for (int j = 0; j < line.length; j++) {
-                if (line[j] == Board.GOAL_CHAR) {
-                    goal = new Point(i, j);
-                } else if (Character.isDigit(line[j])) {
-                    int number = Character.getNumericValue(line[j]);
-                    robots[number] = new Point(i, j);
-                } else if (line[j] != Board.EMPTY_CHAR &&
-                           line[j] != Board.WALL_CHAR) {
-                    System.err.println("Error: Unknown character '" + line[j] + "' on line " + (i+1) + ", column " + (j+1));
-                }
+		for (int i = 0; i < size; i++) {
+			char[] line = in.readLine().toCharArray();
+			for (int j = 0; j < line.length; j++) {
+				if (line[j] == Board.GOAL_CHAR) {
+					goal = new Point(i, j);
+				} else if (Character.isDigit(line[j])) {
+					int number = Character.getNumericValue(line[j]);
+					robots[number] = new Point(i, j);
+				} else if (line[j] != Board.EMPTY_CHAR &&
+						line[j] != Board.WALL_CHAR) {
+					System.err.println("Error: Unknown character '" + line[j] + "' on line " + (i+1) + ", column " + (j+1));
+				}
 
-                b[i][j] = line[j];
-            }
-        }
+				b[i][j] = line[j];
+			}
+		}
 
-        if (goal == null) {
-            System.err.println("Error: No goal found on board");
-        }
+		if (goal == null) {
+			System.err.println("Error: No goal found on board");
+		}
 
-        return new Board(b, robots, goal);
-    }
+		return new Board(b, robots, goal);
+	}
 }
